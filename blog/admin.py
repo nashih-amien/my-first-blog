7947bab17eee252b1470f342c54344df40415544
nashih-amien <nashih.amien@gmail.com>
2024-10-01 13:14:19 +0700

    add models

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, comment
+from .models import Post, comment, Tag
 
 admin.site.register(Post)
 admin.site.register(comment)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,17 @@ from django.db import models
 from django.utils import timezone
 
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    tag = models.ManyToManyField(Tag)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -22,10 +27,13 @@ class Post(models.Model):
 
 class comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
     name = models.CharField(max_length=20)
-    content = models.TextField(max_length=500)
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.post}: {self.content[:50]}"
 
+class Profile(models.Model):
+    user = models.OneToOneField(Post, on_delete=models.CASCADE)
+    bio = models.TextField()
