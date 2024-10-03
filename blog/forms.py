8b6dea2e4e48a88cf972b7a1ae74ba6834d78bf3
nashih-amien <nashih.amien@gmail.com>
2024-10-03 09:02:41 +0700

    add validation forms

--- blog/forms.py
+++ blog/forms.py
@@ -8,6 +8,17 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text', 'tag')
 
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if any(char.isdigit() for char in title):
+            raise forms.ValidationError("Title tidak boleh mengandung angka.")
+        return title
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get('title')
+        content = cleaned_data.get('content')
+
 class PostEdit(forms.ModelForm):
 
     class Meta:
