913029ffbf7551e94732c6b7ba765bd8bec14ff0
nashih-amien <nashih.amien@gmail.com>
2024-10-03 09:26:20 +0700

    add validation forms 2

--- blog/forms.py
+++ blog/forms.py
@@ -13,11 +13,15 @@ class PostForm(forms.ModelForm):
         if any(char.isdigit() for char in title):
             raise forms.ValidationError("Title tidak boleh mengandung angka.")
         return title
-
     def clean(self):
         cleaned_data = super().clean()
         title = cleaned_data.get('title')
         content = cleaned_data.get('content')
+
+        # Jika panjang title lebih dari 30, maka panjang content tidak boleh lebih dari 50
+        if title is not None and content is not None:
+            if len(title) > 30 and len(content) > 50:
+                self.add_error('content', "Jika title lebih dari 30 karakter, content tidak boleh lebih dari 50 karakter.")
 
 class PostEdit(forms.ModelForm):
 
